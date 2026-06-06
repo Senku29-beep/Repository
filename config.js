a2df2905189290fc1b42636f822c4559e6837b96
Senku29-beep <aleksejsmurygin29@gmail.com>
2026-06-06 21:00:50 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// ========== GitHub СИНХРОНИЗАЦИЯ ==========
-const GITHUB_TOKEN = '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';
-const GIST_ID = '352bdb9b6b35d9604caff6399093b8b4';
-const API_URL = `https://api.github.com/gists/${GIST_ID}`;
