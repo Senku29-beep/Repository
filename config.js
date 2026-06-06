885b4b35d61ce3ba0ce08ec1ee65c050d2ac414d
Senku29-beep <aleksejsmurygin29@gmail.com>
2026-06-06 20:41:07 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// ========== GitHub СИНХРОНИЗАЦИЯ ==========
+const GITHUB_TOKEN = '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';
+const GIST_ID = '352bdb9b6b35d9604caff6399093b8b4';
+const API_URL = `https://api.github.com/gists/${GIST_ID}`;
